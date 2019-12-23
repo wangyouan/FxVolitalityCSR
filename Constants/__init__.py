edbbdf394d245d81bfa246077f377cd657db6774
Mark Wang <wangyouan0629@hotmail.com>
2019-12-23 20:53:34 +0800

    add some constants

--- Constants/__init__.py
+++ Constants/__init__.py
@@ -17,8 +17,17 @@ class Constants(PathInfo):
     TICKER = 'tic'
     SIC_CODE = 'sic'
     CUSIP8 = 'cusip8'
+    COUNTRY2 = 'country2'
+    COUNTRY = 'country'
 
     ROA = 'roa'
+
+    # Fixed effects
+    FIRM_FE = 'firm_fe'
+    INDUSTRY_FE = 'industry_fe'
+    INDUSTRY_YEAR_FE = 'industry_year_fe'
+    COUNTRY_FE = 'country_fe'
+    COUNTRY_YEAR_FE = 'country_year_fe'
 
 
 if __name__ == '__main__':
