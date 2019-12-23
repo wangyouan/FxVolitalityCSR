630e43fd57a35a28e24d4af4418bf45530f494fd
Mark Wang <wangyouan0629@hotmail.com>
2019-12-23 14:37:02 +0800

    restore a4 score

--- Constants/__init__.py
+++ Constants/__init__.py
@@ -12,6 +12,11 @@ from .path_info import PathInfo
 class Constants(PathInfo):
     CUSIP = 'cusip'
     ISIN = 'ISIN'
+    YEAR = 'year'
+    GVKEY = 'gvkey'
+    TICKER = 'tic'
+    SIC_CODE = 'sic'
+    CUSIP8 = 'cusip8'
 
 
 if __name__ == '__main__':
---
+++ SortData/OrganizeCSRScore/step01_restrore_a4_score.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: step01_restrore_a4_score
+# @Date: 2019/12/23
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
+
+"""
+python -m SortData.OrganizeCSRScore.step01_restrore_a4_score
+"""
+
+import os
+
+import pandas as pd
+from pandas import DataFrame
+from tqdm import tqdm
+
+from Constants import Constants as const
+
+if __name__ == '__main__':
+    tqdm.pandas()
+    raw_score_path = os.path.join(const.DATABASE_PATH, 'TR', 'asset4', 'raw_score')
+    symbel_xlsx = pd.ExcelFile(os.path.join(const.DATABASE_PATH, 'TR', 'A4_symbols.xlsx'))
+    symbel_dfs = list()
+    for sheet_name in tqdm(symbel_xlsx.sheet_names):
+        symbel_df: DataFrame = symbel_xlsx.parse(sheet_name, skiprows=1)
+        symbel_dfs.append(symbel_df)
+
+    symbel_df: DataFrame = pd.concat(symbel_dfs, ignore_index=True).dropna(axis=1, how='all')
+
+    a4_dfs = list()
+    error_isin = list()
+    for f in tqdm(os.listdir(raw_score_path)):
+        a4_df: DataFrame = pd.read_pickle(os.path.join(raw_score_path, f)).rename(columns={'Instrument': const.ISIN})
+        if const.ISIN not in a4_df.keys():
+            error_isin.append(f)
+            continue
+        a4_df.loc[:, 'Date'] = pd.to_datetime(a4_df['Date'], format='%Y-%m-%dT%H:%M:%SZ')
+        a4_dfs.append(a4_df)
+
+    a4_df: DataFrame = pd.concat(a4_dfs, ignore_index=True)
+    other_keys = [i for i in a4_df.keys() if i not in {const.ISIN, 'Date'}]
+    a4_df_valid: DataFrame = a4_df.dropna(subset=other_keys, how='all')
+    a4_df_valid.loc[:, const.YEAR] = a4_df_valid['Date'].dt.year
+
+    pd.to_pickle(error_isin, '20191223_error_isin_code.pkl')
