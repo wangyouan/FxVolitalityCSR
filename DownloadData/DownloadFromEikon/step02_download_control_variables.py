2b58c97b083754f1cfd3ae11986744034b9a3bdd
Mark <wangyouan0629@hotmail.com>
2019-12-24 11:16:37 +0800

    fix error

--- DownloadData/DownloadFromEikon/step02_download_control_variables.py
+++ DownloadData/DownloadFromEikon/step02_download_control_variables.py
@@ -29,11 +29,15 @@ if __name__ == '__main__':
         os.makedirs(save_path)
 
     ek.set_app_key('b523a56d9c824a578861a38e8e11ac084cf2a8a6')
+    error_list = list()
 
     for f in tqdm(os.listdir(os.path.join(a4_score_path, 'a4s'))):
         if os.path.isfile(os.path.join(save_path, f)):
             pre_df: DataFrame = pd.read_pickle(os.path.join(save_path, f))
             pre_df_valid: DataFrame = pre_df.dropna(subset=[i for i in pre_df.keys() if i != 'Instrument'], how='all')
+            if 'Date' not in pre_df_valid.keys():
+                error_list.append(f)
+                continue
             if pre_df_valid.loc[pre_df_valid['Date'].isnull()].empty:
                 continue
             identifier = pre_df_valid.iloc[0]['Instrument']
@@ -43,3 +47,4 @@ if __name__ == '__main__':
         df, err = ek.get_data(identifier, CTRL_VARS,
                               {'SDate': 0, 'EDate': -19, 'FRQ': 'FY', 'RH': 'Fd'})
         df.to_pickle(os.path.join(save_path, f))
+        print(error_list)
