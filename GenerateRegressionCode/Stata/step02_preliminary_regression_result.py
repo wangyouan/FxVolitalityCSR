34061b6389e19f37a17a3b2c7b66aaef825ed774
Mark Wang <wangyouan0629@hotmail.com>
2020-02-14 14:34:40 +0800

    test new controls

--- GenerateRegressionCode/Stata/step02_preliminary_regression_result.py
+++ GenerateRegressionCode/Stata/step02_preliminary_regression_result.py
@@ -19,7 +19,7 @@ DEP_VARS = ['ESG_Score', 'ESG_Controversies_Score', 'ES_Score', 'ENV_Score', 'SO
             'Community_Score', 'Human_Rights_Score', 'Product_Responsibility_Score', 'Management_Score',
             'Shareholders_Score', 'CSR_Strategy_Score']
 
-CTRL_VARS = ['GDP_LN GDP_GROWTH GDP_CAP_LN IMPORT_RATIO EXPORT_RATIO INTEREST_RATE STOCK_RATIO CAPITAL_RATIO',
+CTRL_VARS = ['GDP_LN GDP_GROWTH GDP_CAP_LN IMPORT_RATIO EXPORT_RATIO',
              'LEVERAGE', 'EBITDA', 'EBITDA_SIGMA', 'FIRM_AGE', 'at_ln']
 
 IND_VARS = ['usd_annual_unexpected_garch_vola usd_annual_log_rate',
@@ -33,8 +33,10 @@ IND_VARS = ['usd_annual_unexpected_garch_vola usd_annual_log_rate',
 if __name__ == '__main__':
     date_code = '20200214'
 
-    reg_code_path = os.path.join(const.MINING_CODE_PATH, '{}_preliminary_regression_code.do'.format(date_code))
-    save_path = os.path.join(const.MINING_RESULT_PATH, '{}_combination1')
+    reg_code_path = os.path.join(const.MINING_CODE_PATH, '{}_preliminary_regression_code2.do'.format(date_code))
+    save_path = os.path.join(const.MINING_RESULT_PATH, '{}_combination2'.format(date_code))
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
 
     cmd_list = ['clear', 'use "{}"'.format(os.path.join(const.RESULT_PATH, '20200116_fx_csr_reg_df.dta'))]
     fe_option = 'firm_fe industry_year_fe'
@@ -44,11 +46,13 @@ if __name__ == '__main__':
     for ind in IND_VARS:
         for dep in DEP_VARS:
             cmd_list.append(
-                'capture qui reghdfe {dep} {ind} {ctrl}, absorb({fe}) cluster(firm_fe) keepsingleton'.format(
-                    dep=dep, ind=ind, fe=fe_option, ctrl=' '.join(CTRL_VARS)))
+                'capture qui reghdfe {dep} {ind} {ctrl} {condition}, absorb({fe}) cluster(firm_fe) keepsingleton'.format(
+                    dep=dep, ind=ind, fe=fe_option, ctrl=' '.join(CTRL_VARS),
+                    condition='if basket27_annual_unexpected_garch != .'))
             cmd_list.append('outreg2 using "{save_file}", addtext({text}) {or_option}'.format(
-                save_file=os.path.join(save_path, '{}_fe_reg_result_ind_{}.txt'.format(
-                    date_code, ind.split('_')[0])), text=text_option, or_option=or_option))
+                save_file=os.path.join(save_path, '{}_fe_reg_result_ind_{}_{}.txt'.format(
+                    date_code, ind.split('_')[0], '2' if 'reali' in ind else '1')), text=text_option,
+                or_option=or_option))
             cmd_list.append('')
 
     with open(reg_code_path, 'w') as f:
