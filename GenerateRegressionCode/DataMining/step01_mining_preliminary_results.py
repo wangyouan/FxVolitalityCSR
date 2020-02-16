acba65264b6c2abe2900399e082fd5e9a508499f
Mark Wang <wangyouan0629@hotmail.com>
2020-02-16 17:20:28 +0800

    update data mining code

--- GenerateRegressionCode/DataMining/step01_mining_preliminary_results.py
+++ GenerateRegressionCode/DataMining/step01_mining_preliminary_results.py
@@ -14,20 +14,22 @@ import os
 
 from Constants import Constants as const
 
-DEP_VAR_DICT = {'ESG_Score': -1, 'ESG_Controversies_Score': 1, 'ES_Score': -1, 'ENV_Score': -1, 'SOC_Score': -1,
-                'CGOV_Score': -1, 'Resource_Use_Score': -1, 'Emissions_Score': -1, 'Environmental_Innovation_Score': -1,
-                'Community_Score': -1, 'Human_Rights_Score': -1, 'Product_Responsibility_Score': -1,
-                'Management_Score': -1, 'Shareholders_Score': -1, 'CSR_Strategy_Score': -1, 'CASH_LN': 1,
-                'CASH_RATIO': 1}
-CTRL_VARS = ['GDP_LN GDP_GROWTH GDP_CAP_LN IMPORT_RATIO EXPORT_RATIO', 'at_ln', 'LEVERAGE', 'EBITDA', 'EBITDA_SIGMA',
+# DEP_VAR_DICT = {'ESG_Score': -1, 'ESG_Controversies_Score': 1, 'ES_Score': -1, 'ENV_Score': -1, 'SOC_Score': -1,
+#                 'CGOV_Score': -1, 'Resource_Use_Score': -1, 'Emissions_Score': -1,
+#                 'Environmental_Innovation_Score': -1,
+#                 'Community_Score': -1, 'Human_Rights_Score': -1, 'Product_Responsibility_Score': -1,
+#                 'Management_Score': -1, 'Shareholders_Score': -1, 'CSR_Strategy_Score': -1, 'CASH_LN': 1,
+#                 'CASH_RATIO': 1}
+DEP_VAR_DICT = {'SOC_Score': -1, 'Resource_Use_Score': -1, 'Environmental_Innovation_Score': -1,
+                'Human_Rights_Score': -1, 'CASH_LN': 1, 'CASH_RATIO': 1}
+CTRL_VARS = ['GDP_LN GDP_GROWTH GDP_CAP_LN IMPORT_RATIO EXPORT_RATIO', 'at_ln', 'LEVERAGE', 'PTBI', 'VOL_PTBI',
              'FIRM_AGE']
 
-IND_VARS = ['usd_annual_unexpected_garch_vola usd_annual_log_rate',
-            'basket60_ann_unexp_garch_vol basket60_annual_log_rate']
+IND_VARS = ['usd_annual_unexpected_garch_vola usd_annual_log_rate']
 
 if __name__ == '__main__':
     cmd_list = ['[data]',
-                'file_path = {}'.format(os.path.join(const.RESULT_PATH, '20200116_fx_csr_reg_df.dta')),
+                'file_path = {}'.format(os.path.join(const.RESULT_PATH, '20200216_fx_csr_reg_df_no_basket27.dta')),
                 'entity_column = firm_fe',
                 'time_column = industry_year_fe', '']
 
