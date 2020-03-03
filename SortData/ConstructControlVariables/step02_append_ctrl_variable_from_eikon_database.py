fix kz index calculation error

SortData/ConstructControlVariables/step02_append_ctrl_variable_from_eikon_database.py
@@ -154,10 +154,10 @@ if __name__ == '__main__':
     ctat_df_with_eikon.loc[:, 'FX_EXPO_DUMMY'] = ctat_df_with_eikon['fca'].fillna(0).apply(lambda x: int(x > 0))
     ctat_df_with_eikon.loc[:, 'FIRM_AGE'] = ctat_df_with_eikon['TR_firm_age'].progress_apply(lambda x: np.log(x + 1))
 
-    ctat_df_with_eikon.loc[:, 'KZ_INDEX'] = ctat_df_with_eikon.groupby(const.GVKEY).progress_apply(calculate_kz_index)
+    ctat_df_with_eikon_with_kz = ctat_df_with_eikon.groupby(const.GVKEY).progress_apply(calculate_kz_index)
 
-    ctat_df_with_eikon.to_pickle(os.path.join(const.TEMP_PATH, '20200214_ctat_global_ctrl_vars_t_1.pkl'))
-    ctrl_df: DataFrame = ctat_df_with_eikon.rename(
+    ctat_df_with_eikon_with_kz.to_pickle(os.path.join(const.TEMP_PATH, '20200303_ctat_global_ctrl_vars_t_1.pkl'))
+    ctrl_df: DataFrame = ctat_df_with_eikon_with_kz.rename(
         columns={'isin': 'ISIN'}).loc[:,
                          [const.GVKEY, 'ISIN', const.YEAR, 'CASH_LN', 'CASH_RATIO', 'KZ_INDEX', 'TobinQ', 'EBITDA',
                           'EBITDA_SIGMA', 'LOSS', 'LEVERAGE', 'PTBI', 'VOL_PTBI', 'DELTA_SGA', 'SIZE', 'FIRM_AGE',